aa0fed909067d61a6ad6764191a76b69c90f047b
Michael Thumann <michaelthumann@Michaels-MacBook-Pro.local>
2024-02-06 19:49:17 -0500

    adding operators.py

---
+++ operators.py
@@ -0,0 +1,14 @@
+a = 12
+b = 3
+
+print(a + b)  # 15 addition
+print(a - b)  # 9 subtraction
+print(a * b)  # 36 multiplication
+print(a / b)  # 4.0 division
+print(a // b)  # integer division rounded down towards infinity
+print(a % b)  # 0 modulo: the remainder after integer division
+
+print()
+
+for i in range(1, a // b):
+    print(i)
