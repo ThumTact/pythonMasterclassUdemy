a6367b1b5520d2fd6d37dcc9a6cec3e336479344
thumtact <mthumann6489@gmail.com>
2024-02-10 21:57:18 -0500

    Coding exercise 11 done

---
+++ whileloop.py
@@ -0,0 +1,9 @@
+for i in range(10):
+    print("i is not {}".format(i))
+
+print("-" * 20)
+# while loop with the same functionality as above ^
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1
