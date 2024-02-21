d690c909a5775c21dd07aadf92bd7c531a66af77
thumtact <mthumann6489@gmail.com>
2024-02-20 19:27:46 -0500

    Sequences up through processing a nested list

--- lists_intro.py
+++ lists_intro.py
@@ -4,12 +4,10 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse mat"
                   ]
+print(computer_parts)
+# computer_parts[3] = "trackball"
 
-for part in computer_parts:
-    print(part)
+print(computer_parts[3:])
 
-print()
-print(computer_parts[2])
-
-print(computer_parts[0:3])
-print(computer_parts[-1])
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
--- number_list.py
+++ number_list.py
@@ -1,14 +1,27 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+numbers = [even, odd]
+print(numbers)
 
-print()
-print(len(even))
-print(len(odd))
-
-print()
-print("Mississippi".count("iss"))
+for number_list in numbers:
+    print(number_list)
+    for value in number_list:
+        print(value)
+# numbers = even + odd
+# print(numbers)
+#
+# sorted_numbers = sorted(numbers)
+# print(sorted_numbers)
+# print(numbers)
+#
+# digits = list("432985617")
+# print(digits)
+#
+# # more_numbers = numbers[:]
+# # more_numbers = list(numbers)
+# more_numbers = numbers.copy()
+# print(more_numbers)
+# print(numbers is more_numbers)
+# print(numbers == more_numbers)
